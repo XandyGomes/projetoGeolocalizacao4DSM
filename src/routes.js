9fadaccb6ffd425801718901564035098eb53ef2
XandyGomes <x3.computadores@hotmail.com>
2023-05-29 19:41:00 -0300

    Aula.29.05.2023

---
+++ src/routes.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Main from './pages/main';
+import Login from './pages/login';
+
+const Stack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="login" component={Login} options={{
+          title: 'LOGIN',
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#581C87',
+          },
+          headerTitleStyle: {
+            color: '#fff',
+            fontWeight: 'bold',
+          }
+        }} />
+        <Stack.Screen name='main' component={Main} options={{
+          title: 'GEOLOCALIZAÇÃO',
+          headerLeft: null,
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#581C87',
+          },
+          headerTitleStyle: {
+            color: '#fff',
+            fontWeight: 'bold',
+          }
+        }}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
