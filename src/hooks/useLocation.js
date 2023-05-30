a5540f2cd2201868058212a718747de169b65088
XandyGomes <x3.computadores@hotmail.com>
2023-05-30 19:29:08 -0300

    Aula.30.05.2023

--- src/hooks/useLocation.js
+++ src/hooks/useLocation.js
@@ -14,13 +14,38 @@ export default () => {
           PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
           PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION
         ]).then(
-         (statuses) => {
+          (statuses) => {
             const statusFine = statuses[PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION];
             const statusBack = statuses[PERMISSIONS.ANDROID.ACCESS_BACKGROUND_LOCATION];
-         } 
-        )
 
-    }
-    )();
+            if (Platform.Version < 29) {
+              if (statusFine == 'granted') {
+                return true
+              } else {
+                setErrorMsg('Usuário não aceitou solicitação de uso do GPS');
+              }
+            }
+
+            if (statusFine == 'granted' && statusBack == 'granted') {
+              return true;
+            } else {
+              setErrorMsg('Usuário não aceitou solicitação de uso do GPS');
+            }
+          }
+        )
+      if(result) {
+        await Geolocation.getCurrentPosition(
+          ({ coords }) => {
+            setCoords({
+              latitude: coords.latitude,
+              longitude: coords.longitude,
+            });
+          }, (error) => {
+            setErrorMsg('Não foi possível obter a localização');
+          }, { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000, showLocationDialog: true }
+        );
+      }
+    })();
   }, []);
+  return {coords, errorMsg};
 }
